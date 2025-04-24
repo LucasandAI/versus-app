be6d3b2c3355b367fa48cbdd1f064e35b9c28442
Lucas Gruter <lucasgruter@Lucass-MacBook-Air.local>
2025-04-24 11:19:38 +0200

    fix: make support ticket chat input visible and properly positioned

--- src/components/chat/ChatTicketContent.tsx
+++ src/components/chat/ChatTicketContent.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { SupportTicket } from '@/types/chat';
 import ChatMessages from './ChatMessages';
@@ -14,14 +13,18 @@ const ChatTicketContent = ({ ticket, onSendMessage }: ChatTicketContentProps) =>
   const { currentUser } = useApp();
 
   return (
-    <div className="flex-1 flex flex-col h-full">
-      <ChatMessages 
-        messages={ticket.messages || []} 
-        clubMembers={currentUser ? [currentUser] : []}
-        isSupport={true}
-      />
+    <div className="flex flex-col h-full">
+      <div className="flex-1 overflow-y-auto">
+        <ChatMessages 
+          messages={ticket.messages || []} 
+          clubMembers={currentUser ? [currentUser] : []}
+          isSupport={true}
+        />
+      </div>
       
-      <ChatInput onSendMessage={onSendMessage} />
+      <div className="sticky bottom-0 left-0 right-0 bg-white border-t">
+        <ChatInput onSendMessage={onSendMessage} />
+      </div>
     </div>
   );
 };
